4c787861a0538ca52b54281e71cddbf8e1cc4f24
Konrad Willmert <konrad.willmert@indwes.edu>
2023-12-14 10:17:45 -0500

    Day 6 part 2

--- main06.py
+++ main06.py
@@ -12,20 +12,62 @@ dists = [295,1734,1278,1210]
 def Distance( tHeld, tTotal ):
     return max( 0, tHeld * (tTotal - tHeld) )
 
-result1 = 1
+def FindWins( times, dists ):
+    result1 = 1
 
-for raceNumber in range( len( times ) ):
+    for raceNumber in range( len( times ) ):
 
-    print( "Race #" + str( raceNumber ) )
-    raceTime = times[raceNumber]
-    winCount = 0
+        print( "Race #" + str( raceNumber ) )
+        raceTime = times[raceNumber]
+        winCount = 0
 
-    for tHeld in range( raceTime + 1 ):
-        if Distance( tHeld, raceTime ) > dists[raceNumber]:
-            winCount += 1
+        for tHeld in range( raceTime + 1 ):
+            d = Distance( tHeld, raceTime )
+            # print( d )
+            if d > dists[raceNumber]:
+                winCount += 1
 
-    result1 *= winCount
-    print( "Wins: " + str( winCount ) )
-    print()
+        result1 *= winCount
+        print( "Wins: " + str( winCount ) )
+        print()
 
-print( "Part 1: " + str( result1 ) )
+    return result1
+
+def FindWins2( times, dists ):
+    result1 = 1
+
+    for raceNumber in range( len( times ) ):
+
+        print( "Race #" + str( raceNumber ) )
+        raceTime = times[raceNumber]
+        lossCount = 0
+
+        for tHeld in range( raceTime + 1 ):
+            d = Distance( tHeld, raceTime )
+            # print( d )
+            if d <= dists[raceNumber]:
+                lossCount += 1
+            else:
+                break
+
+        winCount = raceTime + 1 - lossCount * 2
+        result1 *= winCount
+        print( "Wins: " + str( winCount ) )
+        print()
+
+    return result1
+
+
+print( "Part 1: " + str( FindWins( times, dists ) ) )
+print()
+print( "Part 1: " + str( FindWins2( times, dists ) ) )
+print()
+
+# Example
+# times = [71530]
+# dists = [940200]
+
+times = [45988373]
+dists = [295173412781210]
+
+print( "Part 2: " + str( FindWins2( times, dists ) ) )
